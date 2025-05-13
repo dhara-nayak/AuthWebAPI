29e87674a0e5fb5091b438d6d6676d695e85c620
dhara-nayak <dharanayak19@gmail.com>
2025-05-13 12:24:28 +0530

    Add files via upload

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using AuthWebAPIDemo.Entities;
+using AuthWebAPIDemo.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthWebAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private static User? user = new();
+        [HttpPost("Register")]
+        public ActionResult<User?>Register(UserDto request)
+        {
+            user.Username = request.Username;
+            user.PasswordHash = new PasswordHasher<User>()
+                .HashPassword(user, request.Password);
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<User?> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+                return BadRequest("User not found");
+            if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password)
+                == PasswordVerificationResult.Failed)
+                return BadRequest("Wrong PAssword");
+            return Ok(user);
+        }
+    }
+}
---
+++ Model/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthWebAPIDemo.Model
+{
+    public class UserDto //DataTransferObject
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
+ //no need of save data of this only use to share data into API
