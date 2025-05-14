bca04da65c78651d9f5b1779509a71d708d9c882
dhara-nayak <dharanayak19@gmail.com>
2025-05-14 18:20:09 +0530

    Add files via upload

---
+++ Data/MyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using AuthWebAPIDemo.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthWebAPIDemo.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Data/MyDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace AuthWebAPIDemo.Data
+{
+    public class MyDbContextFactory : IDesignTimeDbContextFactory<MyDbContext>
+    {
+        public MyDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()) // Ensure this is where appsettings.json is
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<MyDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new MyDbContext(optionsBuilder.Options);
+        }
+    }
+}
+//To Fix Adding a Design-Time Factory
+//EF core tools dont know how to configure MyDbContext at design time that's wjhy added this extra class into data. 
